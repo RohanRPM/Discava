d27841a399c7ef4606d9c3b31933182f26749f6c
Rohan Meshram <117159898+RohanRPM@users.noreply.github.com>
2024-08-01 19:09:04 +0530

    H

--- src/services/operations/authAPI.js
+++ src/services/operations/authAPI.js
@@ -16,31 +16,31 @@ const {
 
 export function sendOtp(email, navigate) {
   return async (dispatch) => {
-    const toastId = toast.loading("Loading...")
-    dispatch(setLoading(true))
+    const toastId = toast.loading("Loading...");
+    dispatch(setLoading(true));
     try {
+      console.log("Sending OTP to email:", email); 
       const response = await apiConnector("POST", SENDOTP_API, {
         email,
         checkUserPresent: true,
-      })
-      console.log("SENDOTP API RESPONSE............", response)
-
-      console.log(response.data.success)
+      });
+      console.log("SENDOTP API RESPONSE............", response);
 
       if (!response.data.success) {
-        throw new Error(response.data.message)
+        throw new Error(response.data.message);
       }
 
-      toast.success("OTP Sent Successfully")
-      navigate("/verify-email")
+      toast.success("OTP Sent Successfully");
+      navigate("/verify-email");
     } catch (error) {
-      console.log("SENDOTP API ERROR............", error)
-      toast.error("Could Not Send OTP")
+      console.log("SENDOTP API ERROR............", error);
+      toast.error("Could Not Send OTP");
     }
-    dispatch(setLoading(false))
-    toast.dismiss(toastId)
-  }
+    dispatch(setLoading(false));
+    toast.dismiss(toastId);
+  };
 }
+
 
 export function signUp(
   accountType,
